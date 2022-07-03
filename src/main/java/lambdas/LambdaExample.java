5c9775efab20f1fe44ba46db77fd8d8c05b81f3f
Prachi Shukla <76910374+shuklaprachi@users.noreply.github.com>
2022-07-03 23:44:24 +0530

    Update LambdaExample.java

--- src/main/java/lambdas/LambdaExample.java
+++ src/main/java/lambdas/LambdaExample.java
@@ -6,6 +6,6 @@ import java.util.List;
 public class LambdaExample {
     public static void main(String[] args) {
         List<String> list= new ArrayList<>(List.of("ish", "poo", "han", "oooks"));
-        list.stream();
+        list.stream().forEach(System.out::println);
     }
 }
